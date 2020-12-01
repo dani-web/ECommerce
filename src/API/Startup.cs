c8ac4683d4d0c5c49bb6c815c25f0e14ac84c0c4
dani-web <danielhoareau@gmail.com>
2020-12-01 23:11:29 +0100

    End of section 4

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,7 +1,8 @@
-using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -13,7 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using AutoMapper;
 namespace API
 {
     public class Startup
@@ -29,7 +30,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(MappingProfile));
             services.AddScoped<IProductRepository,ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            
             services.AddControllers();
             services.AddDbContext<StoreContext>(x=>
                 x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
@@ -47,7 +51,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
---
+++ src/Core/Specifications/productsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,47 @@
+﻿using Core.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        /*public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }*/
+        public ProductsWithTypesAndBrandsSpecification(int id): base(x=>x.Id==id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+        }
+
+        private void AddOrderByDescending(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ApplyPaging(int v, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddOrderBy(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        private void AddInclude(Func<object, object> p)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
