6286510316897b441430dcc363ffad9ff66b9d7b
dani-web <danielhoareau@gmail.com>
2020-12-03 23:11:44 +0100

    End of section 8

---
+++ src/client/src/app/models/product.ts
@@ -0,0 +1,10 @@
+
+export interface IProduct {
+  id: number;
+  name: string;
+  decription: string;
+  price: number;
+ pictureUrl: string;
+ productBrand: string;
+ productType: string;
+}
---
+++ src/client/src/demo.ts
@@ -0,0 +1,19 @@
+let data: number | string;
+data = '42';
+data = 10;
+interface ICar {
+  color: string;
+  model: string;
+  topSpeed?: number;
+}
+const car1: ICar= {
+  color: 'blue',
+  model: 'bmw',
+
+};
+
+const car2: ICar= {
+  color: 'red',
+  model: 'ferrari',
+ topSpeed: 400,
+};
