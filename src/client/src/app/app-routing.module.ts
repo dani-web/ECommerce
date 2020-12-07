3865c0d1f9c2cd56ba3d614d9c54a9ae9b1e1083
dani-web <danielhoareau@gmail.com>
2020-12-07 08:12:22 +0100

    End of section 10

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ShopComponent},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -7,20 +7,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ShopModule],
+    CoreModule,
+    ShopModule,
+    HomeModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
